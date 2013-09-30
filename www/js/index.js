8bad779938669866aaffb7c90094bf1c06f7df22
Pablo Ramirez <pablillo.es@gmail.com>
2013-09-30 20:18:13 +0200

    insertando imagenes y botones

---
+++ www/js/index.js
@@ -0,0 +1,16 @@
+function cargarInfo1(){
+	var texto="<h1>Documentos compartidos</h1>";
+	texto+="<p>Las empresas que fomentan entre los trabajadores un ambiente de armonía obtienen resultados beneficiosos. El compañerismo se logra cuando hay trabajo y amistad.</p>";
+	texto+="<p>La fuerza que integra al grupo y su cohesión se expresa en la solidaridad y el sentido de pertenencia al grupo que manifiestan sus componentes. Cuanto más cohesión existe, más probable es que el grupo comparta valores, actitudes y normas de conducta comunes.</p>";
+
+	document.getElementById("information").innerHTML=texto;
+}
+
+function cargarInfo2(){
+	var texto="<h1>Trabajo en equipo</h1>";
+	texto+="<p>Las empresas que fomentan entre los trabajadores un ambiente de armonía obtienen resultados beneficiosos. El compañerismo se logra cuando hay trabajo y amistad.</p>";
+	texto+="<p>La fuerza que integra al grupo y su cohesión se expresa en la solidaridad y el sentido de pertenencia al grupo que manifiestan sus componentes. Cuanto más cohesión existe, más probable es que el grupo comparta valores, actitudes y normas de conducta comunes.</p>";
+
+	document.getElementById("information").innerHTML=texto;
+}
+
